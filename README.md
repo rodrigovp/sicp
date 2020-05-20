da40492b5480a687476ddf97231dd0633a526065
Rodrigo Vieira <rodrigovieirapinto@gmail.com>
2020-05-20 20:37:19 -0300

    inclusão de material de apoio e conversão para clisp de código do sub-capítulo 1.1.8

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ _Link_ para uma versão do livro em HTML: https://mitpress.mit.edu/sites/default
 
 _Link_ para _download_ do PDF: https://www.researchgate.net/publication/37597721_Structure_and_Interpretation_of_Computer_Programs_H_Abelson_GJ_Sussman_colaboracion_de_J_Sussman_prol_de_Alan_J_Perlis
 
+Exercícios resolvidos em Common Lisp: https://eli.thegreenplace.net/tag/sicp
+
 _Playlist_ de aulas em vídeo: https://www.youtube.com/playlist?list=PLE18841CABEA24090
 
 Para rodar os scripts, usou-se a ferramenta clisp (https://clisp.sourceforge.io). 
--- aula2.lisp
+++ aula2.lisp
@@ -1,32 +1,26 @@
 ;
-; Contém exercícios do cap 1.2, bem como alguns
+; Contém exercícios e códigos da aula 2, bem como alguns
 ; testes livres.
 ;
 (defun p(x) (print x)) ; alias para a função print
-(defun media(guess x) (/ (+ guess x) 2))
-(p (media 10 0))
 
-(defun quadrado(x) (* x x))
-
-(defun improve(guess x) 
-	(media guess (/ x guess))
-)
-
-(defun bom-o-suficiente?(guess x)
-	(< (abs (- (quadrado guess) x)) 0.001)
-)
-
-(defun raiz-quadrada-iterativa(guess x)
-	(if (bom-o-suficiente? guess x)
-		guess
-		(raiz-quadrada-iterativa (improve guess x) x)
-	)
-)
-(p (raiz-quadrada-iterativa 1 2))
-(defun raiz-quadrada(x) (raiz-quadrada-iterativa 1.0 x))
-(p (raiz-quadrada 2))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(block foo
-	(print 'hello)
-	(return-from foo 43))
+
+(defun quadrado(x) (* x x))
+(defun media(guess x) (/ (+ guess x) 2))
+
+(defun raiz-bloco (x)
+	(defun raiz-quadrada(x) (raiz-quadrada-iterativa 1.0 ))
+	(defun bom-o-suficiente? (guess)(< (abs (- (quadrado guess) x)) 0.001))
+	(defun improve(guess) (media guess (/ x guess)))
+	(defun raiz-quadrada-iterativa(guess)
+		(if (bom-o-suficiente? guess)
+			guess
+			(raiz-quadrada-iterativa (improve guess) )
+		)
+	)
+	(raiz-quadrada 2)
+)
+(p (raiz-bloco 2))
+(p (improve 9 )) ; isso não deveria funcionar! Ou deveria? :O
