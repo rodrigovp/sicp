323d6bdff7e4c1298987799c7bef1000db6105e6
Rodrigo Vieira <rodrigovieirapinto@gmail.com>
2020-07-26 16:01:52 -0300

    remoção de todos os prints

--- aula6.lisp
+++ aula6.lisp
@@ -17,7 +17,7 @@
 	(if (> a b)
 		0
 		(+ (/ 1.0 (* a (+ a 2))) (pi-sum (+ a 4) b))))
-(print (pi-sum 1 3))
+;(print (pi-sum 1 3))
 
 ; mapcar -> aplica uma dada função repetidamente aos argumentos dados
 (defun incrementa(x) (+ x 1))
