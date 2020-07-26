e49922984742a0bfe8ba4f832a7b2f2e82d31d8f
Rodrigo Vieira <rodrigovieirapinto@gmail.com>
2020-07-26 14:36:04 -0300

    Refatoração simples de métodos e testes de high order function com outras funções

--- aula6.lisp
+++ aula6.lisp
@@ -7,21 +7,16 @@
 ;(print (soma-inteiros 3 2)) ;0
 
 (defun cubo(x) (* x x x))
-
 (defun soma-cubos(a b)
 	(if (> a b)
 		0
-		(+ (cubo a) (soma-cubos (+ a 1) b))
-	)
-)
-;(print (soma-cubos 1 3))
+		(+ (cubo a) (soma-cubos (+ a 1) b))))
+(print (soma-cubos 1 3)) ;36
 
 (defun pi-sum(a b)
 	(if (> a b)
 		0
-		(+ (/ 1.0 (* a (+ a 2))) (pi-sum (+ a 4) b))
-	)	
-)
+		(+ (/ 1.0 (* a (+ a 2))) (pi-sum (+ a 4) b))))
 ;(print (pi-sum 2 2))
 
 ;(define (sum term a next b)
@@ -30,18 +25,15 @@
 ;		(+ (term a) (sum term (next a) next b))))
 
 ; mapcar -> aplica uma dada função repetidamente aos argumentos dados
-(defun soma-um(x) 
-	(print (+ x 1))
-)
-;(print (mapcar 'soma-um '(1 2 3 4)))
+(defun incrementa(x) (+ x 1))
+;(print (mapcar 'incrementa '(1 2 3 4)))
 
 ; apply -> aplica uma função a uma lista de argumentos
-;(apply '+ '(1 2 3 4))
+(apply '+ '(1 2 3 4))
 ;(print (apply 'cubo '(2)))
 
+; apenas um teste de uso do comando funcall
 (defun soma (a b) (+ a b))
-;(print (soma 3 2))
-
 (defun chama-funcao-soma (fn x y z)
 	(+ (funcall fn x y) z)
 )
@@ -50,8 +42,8 @@
 (defun funcao-x(fn1 a b)
 	(if (> a b)
 		0
-		(+ (funcall fn1 a) (soma-cubos (+ a 1) b))
+		(+ (funcall fn1 a) (funcao-x fn1 (+ a 1) b))
 	)
 )
-(print (funcao-x #'cubo 1 3))
-
+;(print (funcao-x #'cubo 1 3)) ;36 - igual à função semelhante acima
+;(print (funcao-x #'incrementa 1 3))
