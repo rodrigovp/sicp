a9595eba668cca45fdd4d3a516c1a3393b483f61
Rodrigo Vieira <rodrigovieirapinto@gmail.com>
2020-07-26 17:08:36 -0300

    Resolução de parte do ex. 1.31 e do 1.32

--- aula6.lisp
+++ aula6.lisp
@@ -27,13 +27,6 @@
 (apply '+ '(1 2 3 4))
 ;(print (apply 'cubo '(2)))
 
-; apenas um teste de uso do comando funcall
-(defun soma (a b) (+ a b))
-(defun chama-funcao-soma (fn x y z)
-	(+ (funcall fn x y) z)
-)
-;(print (chama-funcao-soma #'soma 3 2 3))
-
 (defun funcao-pi(a)(/ 1.0 (* a (+ a 2))))
 (defun proximo-pi(a)(+ a 4))
 
@@ -43,7 +36,30 @@
 		(+ (funcall fn1 a) (soma fn1 (funcall fn2 a) b fn2))
 	)
 )
-
+(defun identidade(n) n)
+;(print (soma #'identidade 2 3 #'incrementa)) ;5 - igual à função semelhante acima
 ;(print (soma #'cubo 1 3 #'incrementa)) ;36 - igual à função semelhante acima
 ;(print (soma #'funcao-pi 1 3 #'proximo-pi))
-;(print (* 8 (pi-sum 1 100)))
+;(print (* 8 (pi-sum 1 10000)))
+
+; Exercício 1.31 - primeira e segunda parte do item a
+(defun produto (fn1 a b fn2)
+	(if (> a b)
+		1
+		(* (funcall fn1 a) (produto fn1 (funcall fn2 a) b fn2))
+	)
+)
+;(print (produto #'identidade 1 5 #'incrementa)) ;120
+
+; Exercício 1.32 
+(defun acumulador (fnCombinador valorNulo fn1 a b fn2)
+	(if (> a b)
+		valorNulo
+		;(* (funcall fn1 a) (produto fn1 (funcall fn2 a) b fn2))
+		(funcall fnCombinador (funcall fn1 a) (produto fn1 (funcall fn2 a) b fn2))
+	)
+)
+(defun somar(a b) (+ a b))
+(defun multiplicar (a b) (* a b))
+;(print (acumulador #'somar 0 #'identidade 2 3 #'incrementa)) ;5
+;(print (acumulador #'multiplicar 1 #'identidade 1 5 #'incrementa)) ;120
