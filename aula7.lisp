4fddda666ab88cb54570fc3ef273666a49bb1042
Rodrigo Vieira <rodrigovieirapinto@gmail.com>
2020-08-11 21:44:45 -0300

    Utilização do let

---
+++ aula7.lisp
@@ -0,0 +1,38 @@
+; apenas um teste de utilização de let
+(defun testar-let()
+    (let ((a 55)) 
+        (print a))
+)
+;(testar-let)
+
+(defun proximo-o-suficiente? (x y) (< (abs (- x y)) 0.001))
+(defun positivo? (numero) (> numero 0))
+(defun negativo? (numero) (< numero 0))
+(defun zero? (numero) (= numero 0))
+(defun media (x y) (/ (+ x y) 2))
+;(print (media 10 5))
+
+
+; implementação de parte do capítulo 1.3.3 
+(defun busca (fn ponto-negativo ponto-positivo)
+    (let ((meio (media ponto-negativo ponto-positivo)))
+        (if (proximo-o-suficiente? ponto-negativo ponto-positivo)
+            meio
+            (let ((valor-teste (funcall fn meio)))
+                (cond 
+                    ((positivo? valor-teste) (busca fn ponto-negativo meio))
+                    ((negativo? valor-teste) (busca fn meio ponto-positivo))
+                    ((zero? valor-teste) meio)
+                )
+            )
+        )
+    )
+)
+(print (busca (lambda (x) (- (* x x x) (* 2 x) 3)) 1.0 2.0))
+
+;(defun metodo-biseccao (fn a b)
+;    (let ((valor-a (fn a)))
+;          (valor-b (fn b)))
+;    (cond (test-form-1 form1)
+;          (test-form-2 form2))
+;)
