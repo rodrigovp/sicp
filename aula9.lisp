27351cda41562e4f6d08a22cc2c73aa6c5f7c328
Rodrigo Vieira <rodrigovieirapinto@gmail.com>
2020-09-02 19:53:37 -0300

    Implementação de cons do capítulo 2.1.3 e exercício 2.4

--- aula9.lisp
+++ aula9.lisp
@@ -80,3 +80,41 @@
 )
 ;(print (perimetro-retangulo (criar-ponto 1 1) (criar-ponto 0 0)))
 ;(print (perimetro-retangulo (criar-ponto 10 15) (criar-ponto 20 18)))
+
+; implementação de cons, descrito no capítulo 2.1.3
+(defun cons2 (n1 n2)
+    (lambda (m)
+        (cond ((= m 0) n1)
+              ((= m 1) n2)
+              ((error "valor de m é ~S e deve ser 0 ou 1." m))
+        )
+    )
+)
+(defun car2 (cc)
+    (funcall cc 0))
+
+(defun cdr2 (cc)
+    (funcall cc 1))
+;(print (car2 (cons2 1 2)))
+;(print (cdr2 (cons2 1 2)))
+
+; exercício 2.4
+(defun cons3 (a b) 
+    (lambda (m) (funcall m a b)))
+
+(defun car3 (cc) 
+    (funcall cc (lambda (p q) p)))
+
+(defun cdr3 (cc) 
+    (funcall cc (lambda (p q) q)))
+
+(print 
+    (car3 
+        (cons3 1 2)
+    )
+)
+(print 
+    (cdr3 
+        (cons3 1 2)
+    )
+)
