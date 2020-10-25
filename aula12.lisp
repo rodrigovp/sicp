27448dde2df2b94a00fce60b48b2a07d5539b691
Rodrigo Vieira <rodrigovieirapinto@gmail.com>
2020-10-25 19:19:04 -0300

    Resolução de boa parte do ex. 2.27

--- aula12.lisp
+++ aula12.lisp
@@ -27,4 +27,36 @@
 )
 ;(print (for-each #'print (list 1 2 3 4 5)))
 
+; listp verifica se um elemento é uma lista
+; nlistp é o contrário
+;(defvar numero 1)
+;(defvar lista (list 1 2 3))
+;(print (listp numero))
+;(print (listp lista))
+
+(defun reverter (lista)
+    (defun reverter2 (lista lista-invertida)
+        ;(print lista)
+        ;(print lista-invertida)
+        (if (null lista)
+            lista-invertida
+            (reverter2 (cdr lista) (cons (car lista) lista-invertida))
+        )
+    )
+    (reverter2 lista ())
+)
+;(print (reverter (list 1 2 3 4 5)))
+
+
 ; exercício 2.27
+(defun reversao-profunda (lista)
+    (defun r2 (lista parte-invertida)
+        (if (null lista) 
+            parte-invertida 
+            (r2 (cdr lista) (cons parte-invertida (reverter (car lista))))
+        )
+    )
+    (r2 lista ())
+)
+(print (reversao-profunda '((1 2) (3 4))))
+;(print (reversao-profunda '(1 2 3 4 5)))
